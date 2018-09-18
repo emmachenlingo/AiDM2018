5823cbdd0072c73c51e7fd4eeab4d005d0499e59
yzhang@strw.leidenuniv.nl <chrisamo@163.com>
2018-09-18 11:17:40 +0200

    4 naive approaches

---
+++ ass1/linear.py
@@ -0,0 +1,109 @@
+
+import numpy as np
+import sys
+from time import time
+#record time
+before = time()
+#load data
+'''
+ratings=[]
+f = open("datasets/ratings.dat", 'r')
+for line in f:
+    data = line.split('::')
+    ratings.append([int(z) for z in data[:3]])
+f.close()
+ratings=np.array(ratings)
+'''
+ratings = np.genfromtxt("datasets/ratings.dat", usecols=(0, 1, 2), delimiter='::', dtype='int')
+
+#total number of movies and users
+Nmovie = np.max(ratings[:,1]).astype(int)
+Nuser = np.max(ratings[:,0]).astype(int)
+
+#split data into 5 train and test folds
+nfolds=5
+
+#allocate memory for results. 'rmse' means root mean square error; 'mae' means mean absolute error
+rmse_train=np.zeros(nfolds)
+rmse_test=np.zeros(nfolds)
+
+mae_train=np.zeros(nfolds)
+mae_test=np.zeros(nfolds)
+
+#to make sure the repeatability
+np.random.seed(111)
+
+seqs=[x%nfolds for x in range(len(ratings))]
+np.random.shuffle(seqs)
+
+for fold in range(nfolds):
+    train_sel=np.array([x!=fold for x in seqs])
+    test_sel=np.array([x==fold for x in seqs])
+    train=ratings[train_sel]
+    test=ratings[test_sel]
+
+    #print(train.shape, test.shape)    
+#calculate model parameters: mean rating over the training set:
+    gmr=np.mean(train[:,2])
+
+#R_user is the user averaged ratings
+    train = train[np.argsort(train[:,0])]
+    items = set(train[:,0])
+    R_user = np.zeros(Nuser)
+
+    for x in range(1,Nuser+1):
+        if x in items:
+            R_user[x-1] = np.mean(train[train[:,0]==x, 2])
+        else:
+            R_user[x-1] = gmr
+
+#R_movie is the movie averaged ratings
+    train = train[np.argsort(train[:,1])]
+    items = set(train[:,1])
+    R_movie = np.zeros(Nmovie)
+
+    for x in range(1,Nmovie+1):
+        if x in items:
+            R_movie[x-1] = np.mean(train[train[:,1]==x, 2])
+        else:
+            R_movie[x-1] = gmr
+
+    X = np.vstack([R_user[train[:,0]-1], R_movie[train[:,1]-1], gmr*np.ones(train.shape[0])]).T
+    y = train[:,2]
+    S = np.linalg.lstsq(X,y)
+    
+    pred = np.dot(X, S[0][:,np.newaxis]).flatten()  
+
+    X_test = np.vstack([R_user[test[:,0]-1], R_movie[test[:,1]-1], gmr*np.ones(test.shape[0])]).T
+    pred_test = np.dot(X_test, S[0][:,np.newaxis]).flatten()
+
+#with round
+    pred[pred>5] = 5
+    pred[pred<1] = 1
+    pred_test[pred_test>5] = 5
+    pred_test[pred_test<1] = 1
+
+#calculate 
+#apply the model to the train set:
+    rmse_train[fold]=np.sqrt(np.mean((train[:,2]-pred)**2))
+    mae_train[fold]=np.mean(np.abs(train[:,2]-pred))
+#apply the model to the test set:
+    rmse_test[fold]=np.sqrt(np.mean((test[:,2]-pred_test)**2))
+    mae_test[fold]=np.mean(np.abs(test[:,2]-pred_test))
+    
+#print errors:
+    print("Fold " + str(fold) + ": RMSE_train=" + str(rmse_train[fold]) + "; RMSE_test=" + str(rmse_test[fold]))
+    print("Fold " + str(fold) + ": MAE_train=" + str(mae_train[fold]) + "; MAE_test=" + str(mae_test[fold]))
+
+#print the final conclusion:
+print("\n")
+print("Root Mean Squared error on TRAIN: " + str(np.mean(rmse_train)))
+print("Root Mean Squared error on  TEST: " + str(np.mean(rmse_test)))
+
+print("Mean Absolute error on TRAIN: " + str(np.mean(mae_train)))
+print("Mean Absolute error on  TEST: " + str(np.mean(mae_test)))
+# Just in case you need linear regression: help(np.linalg.lstsq) will tell you 
+# how to do it!
+
+interval = time() - before
+print("Time interval (s):{0}".format(interval))
---
+++ ass1/user_ave_rating.py
@@ -0,0 +1,101 @@
+
+"""
+Created on Fri Sep 18 21:09:10 2015
+
+@author: Wojtek Kowalczyk
+
+This script demonstrates how implement the "global average rating" recommender 
+and validate its accuracy with help of 5-fold cross-validation.
+
+"""
+
+import numpy as np
+import sys
+from time import time
+before = time()
+#load data
+#ratings=read_data("ratings.dat")
+ratings=[]
+f = open("datasets/ratings.dat", 'r')
+for line in f:
+    data = line.split('::')
+    ratings.append([int(z) for z in data[:3]])
+f.close()
+ratings=np.array(ratings)
+
+"""
+Alternatively, instead of reading data file line by line you could use the Numpy
+genfromtxt() function. For example:
+
+ratings = np.genfromtxt("ratings.dat", usecols=(0, 1, 2), delimiter='::', dtype='int')
+
+will create an array with 3 columns.
+
+Additionally, you may now save the rating matrix into a binary file 
+and later reload it very quickly: study the np.save and np.load functions.
+"""
+Nmovie = np.max(ratings[:,1]).astype(int)
+Nuser = np.max(ratings[:,0]).astype(int)
+
+#split data into 5 train and test folds
+nfolds=5
+
+#allocate memory for results:
+rmse_train=np.zeros(nfolds)
+rmse_test=np.zeros(nfolds)
+
+mae_train=np.zeros(nfolds)
+mae_test=np.zeros(nfolds)
+
+#to make sure you are able to repeat results, set the random seed to something:
+np.random.seed(111)
+
+seqs=[x%nfolds for x in range(len(ratings))]
+np.random.shuffle(seqs)
+
+#for each fold:
+for fold in range(nfolds):
+    train_sel=np.array([x!=fold for x in seqs])
+    test_sel=np.array([x==fold for x in seqs])
+    train=ratings[train_sel]
+    test=ratings[test_sel]
+
+    #print(train.shape, test.shape)    
+#calculate model parameters: mean rating over the training set:
+    gmr=np.mean(train[:,2])
+
+    train = train[np.argsort(train[:,0])]
+
+    items = set(train[:,0])
+    mar = np.zeros(Nuser)
+
+    for x in range(1,Nuser+1):
+        if x in items:
+            mar[x-1] = np.mean(train[train[:,0]==x, 2])
+        else:
+            mar[x-1] = gmr
+
+#calculate 
+#apply the model to the train set:
+    rmse_train[fold]=np.sqrt(np.mean((train[:,2]-mar[train[:,0]-1])**2))
+    mae_train[fold]=np.mean(np.abs(train[:,2]-mar[train[:,0]-1]))
+#apply the model to the test set:
+    rmse_test[fold]=np.sqrt(np.mean((test[:,2]-mar[test[:,0]-1])**2))
+    mae_test[fold]=np.mean(np.abs(test[:,2]-mar[test[:,0]-1]))
+    
+#print errors:
+    print("Fold " + str(fold) + ": RMSE_train=" + str(rmse_train[fold]) + "; RMSE_test=" + str(rmse_test[fold]))
+    print("Fold " + str(fold) + ": MAE_train=" + str(mae_train[fold]) + "; MAE_test=" + str(mae_test[fold]))
+
+#print the final conclusion:
+print("\n")
+print("Root Mean Squared error on TRAIN: " + str(np.mean(rmse_train)))
+print("Root Mean Squared error on  TEST: " + str(np.mean(rmse_test)))
+
+print("Mean Absolute error on TRAIN: " + str(np.mean(mae_train)))
+print("Mean Absolute error on  TEST: " + str(np.mean(mae_test)))
+# Just in case you need linear regression: help(np.linalg.lstsq) will tell you 
+# how to do it!
+
+interval = time() - before
+print("Time interval (s):{0}".format(interval))
